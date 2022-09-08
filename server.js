895e26a6aa8a9a21373e2ef4ed4fbdcb9d11a6ad
orionstar0828 <orionstar0828@gmail.com>
2022-09-08 15:50:27 +0200

    Change: port

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const Fastify = require("fastify")
-const userRoute = require('./userRoute')
-const dbConnector = require('./dbConnector')
-const jwtPlugin = require('./jwtPlugin')
-const corsPlugin = require("./corsPlugin")
+const Fastify = require("fastify");
+const userRoute = require("./userRoute");
+const dbConnector = require("./dbConnector");
+const jwtPlugin = require("./jwtPlugin");
+const corsPlugin = require("./corsPlugin");
 
 const fastify = Fastify({
-  logger: true
-})
+    logger: true,
+});
 
-fastify.get('/', async (request, reply) => {
-  reply.send({ hello: "PDS" })
-})
-fastify.register(dbConnector)
+fastify.get("/", async (request, reply) => {
+    reply.send({ hello: "PDS" });
+});
+fastify.register(dbConnector);
 
-fastify.register(corsPlugin)
+fastify.register(corsPlugin);
 
-fastify.register(jwtPlugin)
+fastify.register(jwtPlugin);
 
-fastify.register(userRoute)
+fastify.register(userRoute);
 
 const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+    try {
+        await fastify.listen(process.env.PORT || 3000);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
