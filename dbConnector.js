0b0a56a86d4db3d2ba65ff96d5e47ae08685d9ae
orionstar0828 <orionstar0828@gmail.com>
2022-09-08 15:45:46 +0200

    Change: mongodb url

--- dbConnector.js
+++ dbConnector.js
@@ -1,11 +1,11 @@
-const fastifyPlugin = require('fastify-plugin')
-const fastifyMongodb = require('@fastify/mongodb')
+const fastifyPlugin = require("fastify-plugin");
+const fastifyMongodb = require("@fastify/mongodb");
 
 const dbConnector = async (fastify, options) => {
     fastify.register(fastifyMongodb, {
         forceClose: true,
-        url: "mongodb+srv://orion:orion123!@#@cluster0.gtyrzh5.mongodb.net/test_database"
-    })    
-}
+        url: "mongodb+srv://orion:orion123!%40%23@cluster0.gtyrzh5.mongodb.net/test_database",
+    });
+};
 
-module.exports = fastifyPlugin(dbConnector)
+module.exports = fastifyPlugin(dbConnector);
